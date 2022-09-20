00268b2d0a4d4b3c7736406456d1e74bd3514f29
Darby Smith <newman.darby2@gmail.com>
2022-09-20 11:35:12 -0400

    Add check_stock test and add items to before block

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,8 @@ require './lib/vendor'
 RSpec.describe Vendor do
   before (:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   describe '#initialize' do
@@ -24,12 +26,20 @@ RSpec.describe Vendor do
   describe '#stock' do
     it 'can add given item and amount to the inventory' do
       @vendor.stock(@item1, 30)
-
       expect(@vendor.inventory).to eq({@item1 => 30})
+
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq({@item1 => 30, @item2 => 12})
     end
   end
   
-  # describe '#check_stock' do
-
-  # end
+  describe '#check_stock' do
+    it 'can check the stock of a given item' do
+      @vendor.stock(@item1, 30)
+      expect(@vendor.check_stock(@item1)).to eq(30)
+      
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+  end
 end
