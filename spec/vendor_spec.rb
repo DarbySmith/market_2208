a9803dbac3d7999a19456a851c8c1a6c74d8671f
Darby Smith <newman.darby2@gmail.com>
2022-09-20 11:17:25 -0400

    Add tests for initialize

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,6 +4,20 @@ require './lib/vendor'
 
 RSpec.descirbe Vendor do
   before (:each) do
-    
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@vendor).to be_instance_of(Vendor)
+    end
+
+    it 'has a name' do
+      expect(@vendor.name).to eq("Rocky Mountain Fresh")
+    end
+
+    it 'has no inventory to start' do
+      expect(@vendor.inventory).to eq({})
+    end
   end
 end
