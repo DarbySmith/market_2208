603a9ce394a470152576796ab2fa25538af361eb
Darby Smith <newman.darby2@gmail.com>
2022-09-20 12:16:26 -0400

    Add and pass tests for add_vendor, vendor_names, and vendors_that_sell

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -28,4 +28,43 @@ RSpec.describe Market do
       expect(@market.vendors).to eq([])
     end
   end
+
+  describe '#add_vendor' do
+    it 'can add vendors to the market' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+      # @vendor1.stock(@item1, 35)
+      # @vendor1.stock(@item2, 7)
+      # @vendor2.stock(@item4, 50)
+      # @vendor2.stock(@item3, 25)
+    end
+  end
+  
+  describe '#vendor_names' do
+  it 'returns an array of the vendors names' do   
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+end
+
+describe '#vendors_that_sell' do
+  it 'returns array of vendors that sell the given item' do
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+    expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    end
+  end
 end
